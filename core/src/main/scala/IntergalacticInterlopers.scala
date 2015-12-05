093a97c7bccbf53f7a8f327a7539a93627d9479c
Rob Schluntz <saitou1024@gmail.com>
2015-12-05 16:24:30 -0500

    Merge branch 'ui-net' into uitesting
    # Conflicts:
    #	core/src/main/scala/IntergalacticInterlopers.scala

--- core/src/main/scala/IntergalacticInterlopers.scala
+++ core/src/main/scala/IntergalacticInterlopers.scala
@@ -1,5 +1,7 @@
 package com.github.fellowship_of_the_bus.interlopers
 
+import java.net.InetAddress
+
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.graphics._
 import com.badlogic.gdx.graphics.glutils._
@@ -10,58 +12,83 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextField._
 import com.badlogic.gdx.scenes.scene2d.utils._
 import com.badlogic.gdx.scenes.scene2d._
 
+import com.github.fellowship_of_the_bus.lib.net._
+
+case class Player(fileName: String, var x: Int, var y: Int) {
+  val texture = new Texture(fileName)
+}
+
 class IntergalacticInterlopers extends Game {
   class DemoScreen extends Screen {
     val batch = new SpriteBatch;
     val camera = new OrthographicCamera();
     camera.setToOrtho(false, 800, 480);
     camera.update();
-    val player = new Texture("img/Player.png");
-    val playerR = new Texture("img/PlayerR.png");
+
+    val players = Array(Player("img/Player.png", 50, 50), 
+      Player("img/PlayerR.png", 150, 150))
     val shapeRenderer = new ShapeRenderer
     shapeRenderer.setProjectionMatrix(camera.combined)
-    var x = 50;
-    var y = 50;
 
     var xR = 150;
     var yR = 150;
     val stage = new Stage()
     Gdx.input.setInputProcessor(stage)
 
-    val region = new TextureRegionDrawable(new TextureRegion(player))
-    val region2 = new TextureRegionDrawable(new TextureRegion(playerR))
+    val region = new TextureRegionDrawable(new TextureRegion(players(0).texture))
+    val region2 = new TextureRegionDrawable(new TextureRegion(players(1).texture))
     val font = new BitmapFont()
     val tf = new TextField("hello", new TextFieldStyle(font, Color.WHITE, region, region2, region))
     tf.setBlinkTime(5 * 1/60)
     stage.addActor(tf);
 
+    var me = -1
+    var gameStarted = false
+    var isHost = false
+    var isClient = false
+
+    val socket = new UDPSocket(12345, 512)
+
     def render(delta: Float) = {
       Gdx.gl.glClearColor(0, 0, 0, 1)
       Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
       Gdx.gl.glLineWidth(20)
+      if (gameStarted) {
 
-      
+        {
+          val p = players(me)
+          if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            p.y = p.y + 5;
+          } else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            p.y = p.y - 5;
+          }
 
-      if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
-        y = y + 5;
-      } else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-        y = y - 5;
+          if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            p.x = p.x - 5;
+          } else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            p.x = p.x + 5;
+          }
+        }
+
+        stage.draw()
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        for (p <- players) {
+          batch.draw(p.texture, p.x, p.y)
+        }
+        batch.end();
+      } else if (Gdx.input.isKeyPressed(Input.Keys.H)) {
+        isHost = true
+      } else if (Gdx.input.isKeyPressed(Input.Keys.C)) {
+        isClient = true
+      } else if (isHost) {
+          val font = new BitmapFont()
+          batch.begin()
+          font.draw(batch, s"IP: ${InetAddress.getLocalHost.getHostAddress}", 100, 100)
+          batch.end()
+      } else if (isClient) {
+        
       }
-
-      if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-        x = x - 5;
-      } else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-        x = x + 5;
-      }
-
-      batch.setProjectionMatrix(camera.combined);
-      stage.draw()
-      stage.act()
-      batch.begin();
-      batch.draw(player, x, y)
-      batch.draw(playerR, xR, yR)
-      batch.end();
-
     }
 
     def resize(width: Int, height: Int) = {}
@@ -71,6 +98,7 @@ class IntergalacticInterlopers extends Game {
     def resume() = {}
     def dispose() = {}
   }
+
   override def create() = {
     this.setScreen(new DemoScreen)
 
